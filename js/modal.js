cf3d3a565d346ef3ecc01523df506bcbb22b0874
Mauro Frappietro <45306301+dashurry@users.noreply.github.com>
2022-10-07 11:23:27 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,15 @@
-var modal=document.querySelector("#my-modal"),modalBtn=document.querySelector("#modal-btn"),closeBtn=document.querySelector(".close");modalBtn.addEventListener("click",openModal);closeBtn.addEventListener("click",closeModal);window.addEventListener("click",outsideClick);function openModal(){modal.style.display="block"}function closeModal(){modal.style.display="none"}function outsideClick(a){a.target==modal&&(modal.style.display="none")};
+var modal = document.querySelector("#my-modal"),
+    modalBtn = document.querySelector("#modal-btn"),
+    closeBtn = document.querySelector(".close");
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
+function openModal() {
+    modal.style.display = "block";
+}
+function closeModal() {
+    modal.style.display = "none";
+}
+function outsideClick(a) {
+    a.target == modal && (modal.style.display = "none");
+}
